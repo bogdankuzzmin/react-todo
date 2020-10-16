bb031f33af154c8535c73a238c12728b5853ced2
Bogdan <bogdankuzzmin@gmail.com>
2020-10-16 23:22:16 +0600

    added toggleHandlers (done, important)

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,13 +3,19 @@ import React from "react";
 import TodoListItem from "../todo-list-item/todo-list-item";
 import './todo-list.css';
 
-const TodoList = ({todos, onDeleted}) => {
+const TodoList = ({todos, onDeleted, toggleImportantHandler, toggleDoneHandler}) => {
 const elements = todos.map((item) => { 
   const {id, ...itemProps} = item;
 
   return (
     <li key={id} className="list-group-item todo-list">
-      <TodoListItem {...itemProps} onDeleted={() => onDeleted(id)} />
+      <TodoListItem 
+        {...itemProps} 
+        onDeleted = {() => onDeleted(id)} 
+        toggleImportantHandler = {() => toggleImportantHandler(id)}
+        toggleDoneHandler = {() => toggleDoneHandler(id)}
+
+      />
     </li>
   );
 });
