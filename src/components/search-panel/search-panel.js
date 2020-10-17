2a00094bdc8f0131a53ae1f51996deba65d4d626
Bogdan <bogdankuzzmin@gmail.com>
2020-10-17 23:38:18 +0600

    added logic to search tasks

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,9 +1,26 @@
-import React from "react";
+
+import React, {Component} from 'react';
 
 import './search-panel.css';
 
-const SearchPanel = () => {
-  return <input type="text" className="form-control search-input" placeholder="type to search" />;
-}
+export default class SearchPanel extends Component {
+  state = {
+    term: '',
+  };
 
-export default SearchPanel;
+  searchChangeHandler = (evt) => {
+    const term = evt.target.value
+    this.setState({term});
+    this.props.searchChangeHandler(term);
+  };
+
+  render() {
+    return (
+      <input type="text" 
+             className="form-control search-input" 
+             placeholder="type to search" 
+             value={this.state.term} 
+             onChange={this.searchChangeHandler} />
+    );
+  }
+}
